9e99e8fa1d9143980dbcb62b624f524d86fb40c6
Arie Ridwan <arieridwan@Aries-Air.fritz.box>
2023-04-19 10:55:59 +0200

    Update the content to Game of Life

--- sketch.js
+++ sketch.js
@@ -1,68 +1,90 @@
-let img;
-var w = window.innerWidth;
-var h = window.innerHeight; 
 
-window.onresize = function() {
-  w = window.innerWidth;
-  h = window.innerHeight;  
-  canvas.size(w,h);
-}
+let w;
+let columns;
+let rows;
+let board;
+let next;
 
 function setup() {
-  createCanvas(w, h, WEBGL);
-  img = loadImage('assets/let_it_be.webp');
+  // Set simulation framerate to 10 to avoid flickering
+  frameRate(10);
+  createCanvas(720, 400);
+  w = 20;
+  // Calculate columns and rows
+  columns = floor(width / w);
+  rows = floor(height / w);
+  // Wacky way to make a 2D array is JS
+  board = new Array(columns);
+  for (let i = 0; i < columns; i++) {
+    board[i] = new Array(rows);
+  }
+  // Going to use multiple 2D arrays and swap them
+  next = new Array(columns);
+  for (i = 0; i < columns; i++) {
+    next[i] = new Array(rows);
+  }
+  init();
 }
 
 function draw() {
-  background(0);
+  background(255);
+  generate();
+  for ( let i = 0; i < columns;i++) {
+    for ( let j = 0; j < rows;j++) {
+      if ((board[i][j] == 1)) fill(0);
+      else fill(255);
+      stroke(0);
+      rect(i * w, j * w, w-1, w-1);
+    }
+  }
 
-  let locX = mouseX - height / 2;
-  let locY = mouseY - width / 2;
+}
 
-  ambientLight(60, 60, 60);
-  pointLight(255, 255, 255, locX, locY, 100);
+// reset board when mouse is pressed
+function mousePressed() {
+  init();
+}
 
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  texture(img);
-  box(180);
-  pop();
+// Fill board randomly
+function init() {
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      // Lining the edges with 0s
+      if (i == 0 || j == 0 || i == columns-1 || j == rows-1) board[i][j] = 0;
+      // Filling the rest randomly
+      else board[i][j] = floor(random(2));
+      next[i][j] = 0;
+    }
+  }
+}
 
-  push();
-  translate(-width / 4, -height / 4, 0);
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  fill(250, 0, 0);
-  torus(80, 20, 64, 64);
-  pop();
+// The process of creating the new generation
+function generate() {
 
-  push();
-  translate(width / 4, -height / 4, 0);
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  normalMaterial();
-  torus(80, 20, 64, 64);
-  pop();
+  // Loop through every spot in our 2D array and check spots neighbors
+  for (let x = 1; x < columns - 1; x++) {
+    for (let y = 1; y < rows - 1; y++) {
+      // Add up all the states in a 3x3 surrounding grid
+      let neighbors = 0;
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          neighbors += board[x+i][y+j];
+        }
+      }
 
-  push();
-  translate(-width / 4, height / 4, 0);
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  ambientMaterial(250);
-  torus(80, 20, 64, 64);
-  pop();
+      // A little trick to subtract the current cell's state since
+      // we added it in the above loop
+      neighbors -= board[x][y];
+      // Rules of Life
+      if      ((board[x][y] == 1) && (neighbors <  2)) next[x][y] = 0;           // Loneliness
+      else if ((board[x][y] == 1) && (neighbors >  3)) next[x][y] = 0;           // Overpopulation
+      else if ((board[x][y] == 0) && (neighbors == 3)) next[x][y] = 1;           // Reproduction
+      else                                             next[x][y] = board[x][y]; // Stasis
+    }
+  }
 
-  push();
-  translate(width / 4, height / 4, 0);
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  specularMaterial(250);
-  torus(80, 20, 64, 64);
-  pop();
+  // Swap!
+  let temp = board;
+  board = next;
+  next = temp;
 }
